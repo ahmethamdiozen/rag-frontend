d32194cff52d44547377f2a5453dd51ae674ba24
ahmethamdiozen <hamdiahmetozen@gmail.com>
2026-02-04 22:47:01 +0300

    Initial Next.js frontend for Rag project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,105 @@
-import Image from "next/image";
+"use client"
+
+import { useState } from "react"
+
+type Source = {
+  file_name: string
+  page: number
+}
+
+type AskResponse = {
+  answer: string
+  sources: Source[]
+}
+
 
 export default function Home() {
+  const [question, setQuestion] = useState("")
+  const [answer, setAnswer] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+  const [sources, setSources] = useState<Source[]>([])
+
+
+  async function handleAsk() {
+    setSources([])
+    if (!question.trim()) return
+
+    setLoading(true)
+    setError(null)
+    setAnswer("")
+
+    try {
+      const response = await fetch("http://localhost:8000/ask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          question: question,
+        }),
+      })
+
+      if (!response.ok) {
+        throw new Error("Backend error")
+      }
+
+      const data: AskResponse = await response.json()
+      setAnswer(data.answer)
+      setSources(data.sources)
+    } catch (err) {
+      setError("An error occured")
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="p-8 max-w-2xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">
+        Internal Document Q&A
+      </h1>
+
+      <textarea
+        className="w-full border p-2 mb-4"
+        rows={4}
+        placeholder="Type question here..."
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+      />
+
+      <button
+        onClick={handleAsk}
+        disabled={loading}
+        className="px-4 py-2 bg-black text-white disabled:opacity-50"
+      >
+        {loading ? "Asking..." : "Sor"}
+      </button>
+
+      {error && (
+        <p className="text-red-500 mt-4">{error}</p>
+      )}
+
+      {answer && (
+        <div className="mt-6 p-4 border rounded">
+          <h2 className="font-semibold mb-2">Cevap</h2>
+          <p className="mb-4">{answer}</p>
+
+          {sources.length > 0 && (
+            <div>
+              <h3 className="font-semibold mb-2">Sources</h3>
+              <ul className="list-disc list-inside text-sm text-gray-700">
+                {sources.map((source, index) => (
+                  <li key={index}>
+                    {source.file_name} — Page {source.page}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+      )}
+
+    </main>
+  )
 }
